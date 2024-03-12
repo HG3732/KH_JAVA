8294c9f6ca684843246ba5f917df2f0ec5f0f0fa
HG3732 <gyrua34@gmail.com>
2024-03-12 19:46:59 +0900

    Programmers Lv.1 햄버거만들기 문제 풀이
    https://school.programmers.co.kr/learn/courses/30/lessons/133502

---
+++ Exam/src/programmers/Lv1_BurgerMaking.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lv1_BurgerMaking {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        int[] burger = {1, 2, 3, 1};
+        
+        List<Integer> ing = Arrays.stream(ingredient).boxed().collect(Collectors.toList());
+		
+		for(int i = 0; i<ing.size()-3; i++) {
+			int[] a = {ing.get(i), ing.get(i+1), ing.get(i+2), ing.get(i+3)};
+			 if(Arrays.equals(a, burger)) {
+				 ing.remove(i+3);
+				 ing.remove(i+2);
+				 ing.remove(i+1);
+				 ing.remove(i);
+				 answer++;
+				 if(i>3) {i-=3;}
+				 else {i=0;}
+			 }
+		}
+        return answer;
+    }
+}
