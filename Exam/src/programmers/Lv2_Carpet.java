686990f3d32d1e6f32bec02b601a2094b741dff4
HG3732 <gyrua34@gmail.com>
2024-03-27 20:02:49 +0900

    programmers Lv.2 카펫 풀이

---
+++ Exam/src/programmers/Lv2_Carpet.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class Lv2_Carpet {
+	 public int[] solution(int brown, int yellow) {
+	        int[] answer = {0, 0};
+	        for(int l = 1; l <= (Math.sqrt(yellow)); l++) {
+				if((brown/2)*l - l*(l+2) == yellow) {
+					answer[0] = (yellow/l) +2;
+					answer[1] = l+2;
+					break;
+				}
+	        }
+	        return answer;
+	    }
+}
